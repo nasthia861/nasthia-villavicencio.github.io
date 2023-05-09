71f532e81ea6550d954671ffce4000453c9d791e
Nasthia Villavicencio <nasthia861@gmail.com>
2023-05-09 15:08:07 +0000

    updated lodown index file

--- lodown/index.js
+++ lodown/index.js
@@ -26,16 +26,13 @@ module.exports.each = each;
 /**
  * identity: function takes in an input value and returns value unchanged.
  * 
- * 
  * @param { Any value } value: Function takes in any data type 
  * @returns  { Any value } value: Function returns input change
  */
-
 function identity(value){
     //function should return value
     return value;
 }
-
 module.exports.identity = identity;
 
 /**
@@ -44,7 +41,6 @@ module.exports.identity = identity;
  * @param {Any value} value: Function takes in any data type 
  * @returns {string} string: Function returns the type of value as a string
  */
-
 _.typeOf = function(value){
     //if value is an array return 'array'
     if(Array.isArray(value)){
@@ -55,8 +51,77 @@ _.typeOf = function(value){
         //if value is anything else return value type using typeof
     } else {return typeof value;}
 }
-
 module.exports.typeOf = typeOf;
+
+/**
+ * first: takes an array and a number and returns a list of the first number of values in the array
+ * 
+ * @param {array} array: 
+ * @param {number} number: 
+ * @returns {array} array: 
+ */
+_.first = function(array, number){
+    //create array to push values into
+    var answer = [];
+    //if array argument is an array
+     if(Array.isArray(array)){
+        // if number argument is a number
+         if(typeof number === 'number'){
+            //if number is positive
+            if (number > 0){
+                //if the length of the array argument is bigger than the number argument
+              if(array.length > number){
+                //loop through the array from the first index to 1 less than number
+                    for(let x = 0; x < number; x++){
+                        //push that index into the answer array
+                        answer.push(array[x]);
+                     }
+                //if the number argument is biggger than the array length, return the whole array
+                } else{return array;}
+            }
+        //if number argument is not a number, return the first value in the array   
+         } else{return array[0];}
+    //if number argument is not a number, return []
+     } else{return [];}
+     //return the modified answer array
+    return answer;
+}
+module.exports.first = first;
+
+/**
+ * last: takes an array and a number and returns a list of the last number of values in the array.
+ * 
+ * @param {array} array: an array of values
+ * @param {number} number: any positive number 
+ * @returns {array} array: returns a modified array of the original array
+ */
+_.last = function(array, number){
+    //init answer variable as an array for us to push the answer into
+    var answer = [];
+    //first make sure the array argument is an array
+     if(Array.isArray(array)){
+        //check to see that the number argument is an argument
+         if(typeof number === 'number'){
+            // check that the number argument is positive
+            if (number > 0){
+            // if the length of the array is bigger than the number argument
+              if(array.length > number){
+                    //for loop to go through the array pushing the number of values in the array starting from the end
+                    for(let x = array.length - 1; x > array.length - number - 1; x--){
+                        //unshift method will push each value to the front of the answer array
+                        answer.unshift(array[x]);
+                     }
+                //if the length of the array is less than the number argument, return the full array
+                } else{return array;}
+            }
+        //if the number argument is not a number return the last value in the array   
+         } else{return array[array.length - 1];}
+    //if the array argument is not an array return []
+     } else{return [];}
+     //return modified answer array
+    return answer;
+}
+module.exports.last = last;
 
 /**
  * filter: Function takes in an array of values and a callback function. filter iterates through every value,
